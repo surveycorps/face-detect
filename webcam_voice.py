8ef2ae850c5f9a959747e77b6022d0a20a6a8204
Alfredo Muniz <muniza@seas.upenn.edu>
2015-04-20 09:12:20 +0000

    Fixed relative path to classifier

--- webcam_voice.py
+++ webcam_voice.py
@@ -10,7 +10,7 @@ import speech_recognition as sr
 r = sr.Recognizer()
 
 # Import the Haar Classifier
-cascPath = "haarcascade_frontalface_default.xml"
+cascPath = "apps/face-detect/haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 # Initialize the video capture stream
